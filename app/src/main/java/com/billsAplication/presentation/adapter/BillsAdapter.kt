71a52425c10d2281543c77314fa722b60b308c33
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-03-16 19:44:47 +0700

    Version 1.6 //ViewBinding //Create BillsList ViewModel //Adapter

---
+++ app/src/main/java/com/billsAplication/presentation/adapter/BillsAdapter.kt
@@ -0,0 +1,33 @@
+package com.billsAplication.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import com.billsAplication.R
+import com.billsAplication.domain.model.BillsItem
+
+class BillsAdapter : ListAdapter<BillsItem, BillViewHolder>(BillsListCallback()) { //: ListAdapter<BillsItem, ViewHolder>(BillsListCallback())
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BillViewHolder {
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.bill_item, parent, false)
+        return BillViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holderBill: BillViewHolder, position: Int) {
+        val item = getItem(position)
+
+        holderBill.tv_Day.text = item.date.dropLast(8)
+        holderBill.tv_MonthYear.text = item.date.drop(2)
+        holderBill.tv_Time.text = item.time
+        holderBill.tv_Category.text = item.category
+        holderBill.tv_Item_Description.text = item.description
+
+        if(item.amount > 0){holderBill.tv_Item_Income.text = item.amount.toString() }
+        else { holderBill.tv_Item_Expense.text = item.amount.toString() }
+
+    }
+
+}
---
+++ app/src/main/java/com/billsAplication/presentation/billsList/BillsListViewModel.kt
@@ -0,0 +1,6 @@
+package com.billsAplication.presentation.billsList
+
+import androidx.lifecycle.ViewModel
+
+class BillsListViewModel : ViewModel() {
+}
