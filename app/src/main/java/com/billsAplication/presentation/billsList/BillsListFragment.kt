7a60554ed89ff21034c800a0099e59428965d35b
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-04-19 19:27:58 +0700

    Version 4.2
    //Fix performClick()
    //Add Delete Item

--- app/src/main/java/com/billsAplication/presentation/billsList/BillsListFragment.kt
+++ app/src/main/java/com/billsAplication/presentation/billsList/BillsListFragment.kt
@@ -7,26 +7,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MainThread
 import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.billsAplication.BillsApplication
 import com.billsAplication.R
 import com.billsAplication.databinding.FragmentBillsListBinding
+import com.billsAplication.domain.model.BillsItem
 import com.billsAplication.presentation.adapter.BillsAdapter
+import com.billsAplication.presentation.mainActivity.MainActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 class BillsListFragment : Fragment() {
 
     private var _binding: FragmentBillsListBinding? = null
-    private val binding : FragmentBillsListBinding get() = _binding!!
+    private val binding: FragmentBillsListBinding get() = _binding!!
 
     private val bundle = Bundle()
+
     @Inject
     lateinit var viewModel: BillsListViewModel
+
     @Inject
     lateinit var billAdapter: BillsAdapter
+
+    private var deleteItem = false
+    private var listDeleteItems: ArrayList<BillsItem> = ArrayList()
 
     private val ADD_BILL_KEY = "add_bill_key"
     private val BILL_ITEM_KEY = "bill_item_key"
@@ -52,6 +65,7 @@ class BillsListFragment : Fragment() {
         _binding = FragmentBillsListBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -65,14 +79,14 @@ class BillsListFragment : Fragment() {
             setNewList(binding.tvMonth.text.toString())
         }
 
-        binding.imBackMonth.setOnClickListener{ //Previous month
+        binding.imBackMonth.setOnClickListener { //Previous month
             viewModel.currentDate = viewModel.changeMonthBar(PREV_MONTH)
             binding.tvMonth.text = viewModel.currentDate //Set month`s text in bar
             //set a new list
             setNewList(binding.tvMonth.text.toString())
         }
 
-        binding.imNextMonth.setOnClickListener{ //Next month
+        binding.imNextMonth.setOnClickListener { //Next month
             viewModel.currentDate = viewModel.changeMonthBar(NEXT_MONTH)
             binding.tvMonth.text = viewModel.currentDate //Set month`s text in bar
             //set a new list
@@ -80,13 +94,29 @@ class BillsListFragment : Fragment() {
         }
 
         binding.buttonAddBill.setOnClickListener {
-            bundle.putBoolean(ADD_BILL_KEY, CREATE_TYPE)
-            findNavController().navigate(R.id.action_billsListFragment_to_addBillFragment, bundle)
+            if (deleteItem) {
+                CoroutineScope(Main).launch {
+                    if (listDeleteItems.isNotEmpty()) {
+                        listDeleteItems.forEach {
+                            viewModel.delete(it)
+                        }
+                    }
+                    billAdapter.deleteItems()
+                    deleteItem = false
+                    listDeleteItems.clear()
+                }
+            } else {
+                bundle.putBoolean(ADD_BILL_KEY, CREATE_TYPE)
+                findNavController().navigate(
+                    R.id.action_billsListFragment_to_addBillFragment,
+                    bundle
+                )
+            }
         }
 
         initRecView()
         //set list of month
-        viewModel.list.observe(requireActivity()){
+        viewModel.list.observe(requireActivity()) {
             billAdapter.submitList(it.toList())
         }
     }
@@ -96,8 +126,9 @@ class BillsListFragment : Fragment() {
         _binding = null
     }
 
-    private fun initRecView(){  //TODO Sorting
-        with(binding.recViewBill){
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initRecView() {                              //TODO Sorting
+        with(binding.recViewBill) {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = billAdapter
             binding.recViewBill.itemAnimator = null
@@ -112,16 +143,34 @@ class BillsListFragment : Fragment() {
         }
 
         billAdapter.onLongClickListenerBillItem = {
-            //TODO
+            listDeleteItems = it
+        }
+        //Highlight item
+        billAdapter.isHighlight.observe(requireActivity()) {
+            deleteItem = it
+            if (it) {
+                deleteItem = it
+                binding.buttonAddBill.setImageResource(R.drawable.ic_delete_forever)
+                binding.cardViewBar.visibility = View.GONE
+                binding.cardViewBudget.visibility = View.GONE
+                (activity as MainActivity).findViewById<BottomNavigationView>(R.id.bottom_navigation).visibility =
+                    View.GONE
+            } else {
+                binding.buttonAddBill.setImageResource(R.drawable.ic_add)
+                binding.cardViewBar.visibility = View.VISIBLE
+                binding.cardViewBudget.visibility = View.VISIBLE
+                (activity as MainActivity).findViewById<BottomNavigationView>(R.id.bottom_navigation).visibility =
+                    View.VISIBLE
+            }
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun setNewList(month: String){
+    private fun setNewList(month: String) {
         //set a new list
         viewModel.list.removeObservers(this)
         viewModel.getMonth(month)
-        viewModel.list.observe(requireActivity()){
+        viewModel.list.observe(requireActivity()) {
             billAdapter.submitList(it.toList())
         }
     }
