fa7de931bfed858055467dc9c1c4d814e3582244
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-06-05 17:57:27 +0700

    Version 6.1
     - AnalyticsViewModel
     - Title Bar

--- app/src/main/java/com/billsAplication/presentation/analytics/AnalyticsFragment.kt
+++ app/src/main/java/com/billsAplication/presentation/analytics/AnalyticsFragment.kt
@@ -1,25 +1,51 @@
 package com.billsAplication.presentation.analytics
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.billsAplication.BillsApplication
+import com.billsAplication.R
 import com.billsAplication.databinding.FragmentAnalyticsBinding
+import com.billsAplication.presentation.billsList.BillsListViewModel
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
+import javax.inject.Inject
 
 class AnalyticsFragment : Fragment() {
 
     private var _binding: FragmentAnalyticsBinding? = null
     private val binding: FragmentAnalyticsBinding get() = _binding!!
 
+    @Inject
+    lateinit var viewModel: BillsListViewModel
+
+    private val TYPE_EXPENSES = 0
+    private val TYPE_INCOME = 1
+    private val NEXT_MONTH = true
+    private val PREV_MONTH = false
+
+    private val component by lazy {
+        (requireActivity().application as BillsApplication).component
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
         super.onCreate(savedInstanceState)
     }
 
@@ -31,12 +57,87 @@ class AnalyticsFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initPieChart()
 
         setDataToPieChart()
+
+        titleBar()
+
+
+
+        firstEntrance()
+
+    }
+
+    private fun firstEntrance() {
+        //SetColor state
+        binding.tvExpense.setBackgroundResource(R.drawable.textview_fullbackground_expense)
+        binding.tvIncome.setBackgroundResource(R.drawable.textview_border_income)
+        binding.tvExpense.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+        binding.tvIncome.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_income))
+        binding.tvExpense.isEnabled = false
+        binding.tvIncome.isEnabled = true
+        //set centre title of pieChart
+        binding.pieChart.centerText = "Expense"
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun titleBar() {
+        //Set month`s text in bar
+        binding.tvMonth.text = viewModel.currentDate
+        binding.tvMonth.setOnClickListener {
+            viewModel.currentDate
+            binding.tvMonth.text = viewModel.currentDate()
+            viewModel.defaultMonth()
+            //set a new list
+//            setNewList(binding.tvMonth.text.toString())
+        }
+        //Previous month
+        binding.imBackMonth.setOnClickListener {
+            viewModel.currentDate = viewModel.changeMonthBar(PREV_MONTH)
+            binding.tvMonth.text = viewModel.currentDate //Set month`s text in bar
+            //set a new list
+//            setNewList(binding.tvMonth.text.toString())
+        }
+        //Next month
+        binding.imNextMonth.setOnClickListener {
+            viewModel.currentDate = viewModel.changeMonthBar(NEXT_MONTH)
+            binding.tvMonth.text = viewModel.currentDate //Set month`s text in bar
+            //set a new list
+//            setNewList(binding.tvMonth.text.toString())
+        }
+
+        binding.tvExpense.setOnClickListener{
+            //SetColor state
+            binding.tvExpense.setBackgroundResource(R.drawable.textview_fullbackground_expense)
+            binding.tvIncome.setBackgroundResource(R.drawable.textview_border_income)
+            binding.tvExpense.setTextColor(requireContext().getColorFromAttr(com.google.android.material.R.attr.colorOnPrimary))
+            binding.tvIncome.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_income))
+            binding.tvExpense.isEnabled = false
+            binding.tvIncome.isEnabled = true
+            //set centre title of pieChart
+            binding.pieChart.centerText = "Expense"
+            binding.pieChart.invalidate()
+        }
+
+        binding.tvIncome.setOnClickListener{
+            //SetColor state
+            binding.tvExpense.setBackgroundResource(R.drawable.textview_border_expense)
+            binding.tvIncome.setBackgroundResource(R.drawable.textview_fullbackground_income)
+            binding.tvIncome.setTextColor(requireContext().getColorFromAttr(com.google.android.material.R.attr.colorOnPrimary))
+            binding.tvExpense.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_expense))
+            binding.tvExpense.isEnabled = true
+            binding.tvIncome.isEnabled = false
+            //set centre title of pieChart
+            binding.pieChart.centerText = "Income"
+            binding.pieChart.refreshDrawableState()
+            binding.pieChart.invalidate()
+        }
 
     }
 
@@ -85,15 +186,24 @@ class AnalyticsFragment : Fragment() {
         binding.pieChart.holeRadius = 58f
         binding.pieChart.transparentCircleRadius = 61f
         binding.pieChart.isDrawHoleEnabled = true
-        binding.pieChart.setHoleColor(Color.WHITE)
+        binding.pieChart.setHoleColor(requireContext().getColorFromAttr(com.google.android.material.R.attr.colorOnPrimary))
 
         //add text in center
-        binding.pieChart.setDrawCenterText(true);
-        binding.pieChart.centerText = "Mobile OS Market share"
+        binding.pieChart.setDrawCenterText(true)
+//        binding.pieChart.centerText = "Mobile OS Market share"
 
 
 
         binding.pieChart.invalidate()
+    }
+
+    @ColorInt
+    fun Context.getColorFromAttr(@AttrRes attrColor: Int
+    ): Int {
+        val typedArray = theme.obtainStyledAttributes(intArrayOf(attrColor))
+        val textColor = typedArray.getColor(0, 0)
+        typedArray.recycle()
+        return textColor
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/billsAplication/presentation/analytics/AnalyticsViewModel.kt
+++ app/src/main/java/com/billsAplication/presentation/analytics/AnalyticsViewModel.kt
@@ -1,68 +1,39 @@
-package com.billsAplication.presentation.billsList
+package com.billsAplication.presentation.analytics
 
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.billsAplication.di.ApplicationScope
-import com.billsAplication.domain.billsUseCases.DeleteBillItemUseCase
 import com.billsAplication.domain.billsUseCases.GetAllDataListUseCase
 import com.billsAplication.domain.billsUseCases.GetMonthListUseCase
 import com.billsAplication.domain.billsUseCases.GetTypeUseCase
 import com.billsAplication.domain.model.BillsItem
-import com.billsAplication.presentation.adapter.BillsAdapter
-import java.math.BigDecimal
 import java.time.LocalDate
 import javax.inject.Inject
-import kotlin.coroutines.coroutineContext
 
 @RequiresApi(Build.VERSION_CODES.O)
-@ApplicationScope
-class BillsListViewModel @Inject constructor(
+class AnalyticsViewModel @Inject constructor(
     private val getAllDatabase : GetAllDataListUseCase,
     private val getMonth : GetMonthListUseCase,
-    private val delete : DeleteBillItemUseCase,
     private val getTypeUseCase: GetTypeUseCase
 ) : ViewModel() {
 
     private var date = LocalDate.now()
 
     lateinit var list: LiveData<List<BillsItem>>
-    lateinit var listCategory: LiveData<List<BillsItem>>
 
     var currentDate: String
 
     private var changeMonth = 0
     private var changeYear = 0
 
-    private val TYPE_CATEGORY = 2
-
     init {
         getMonth(currentDate())
         currentDate = currentDate()
-        getCategoryType()
-    }
-
-    fun defaultMonth(){
-        changeMonth = 0
-        changeYear = 0
-    }
-
-    fun getAll() {
-        list = getAllDatabase.invoke()
-    }
-
-    fun getCategoryType() {
-        listCategory = getTypeUseCase.invoke(TYPE_CATEGORY)
     }
 
     fun getMonth(month: String) {
         list =  getMonth.invoke(month)
-    }
-
-    suspend fun delete(item: BillsItem){
-        delete.invoke(item)
     }
 
     fun currentDate() : String{
@@ -81,12 +52,11 @@ class BillsListViewModel @Inject constructor(
         //Set Month
         if (changeMonth > 0) {
             return date.month.plus(changeMonth.toLong()).toString() +
-                        " " + date.year.plus(Math.abs(changeYear).toLong()).toString()
+                    " " + date.year.plus(Math.abs(changeYear).toLong()).toString()
         }
         else {
             return date.month.minus(Math.abs(changeMonth).toLong()).toString() +
-                        " " + date.year.minus(Math.abs(changeYear).toLong()).toString()
-            }
+                    " " + date.year.minus(Math.abs(changeYear).toLong()).toString()
+        }
     }
-
 }
