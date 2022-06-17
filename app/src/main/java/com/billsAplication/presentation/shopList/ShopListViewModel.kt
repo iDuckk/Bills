d96ad1bcd8388b058f6bd77ba71e432911ef39e9
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-06-17 18:01:47 +0700

    Version 6.6
     - speech-to-text

--- app/src/main/java/com/billsAplication/presentation/shopList/ShopListViewModel.kt
+++ app/src/main/java/com/billsAplication/presentation/shopList/ShopListViewModel.kt
@@ -2,14 +2,12 @@ package com.billsAplication.presentation.shopList
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.billsAplication.domain.billsUseCases.DeleteBillItemUseCase
-import com.billsAplication.domain.billsUseCases.GetBookmarksUseCase
-import com.billsAplication.domain.billsUseCases.GetTypeUseCase
-import com.billsAplication.domain.billsUseCases.UpdateBillItemUseCase
+import com.billsAplication.domain.billsUseCases.*
 import com.billsAplication.domain.model.BillsItem
 import javax.inject.Inject
 
 class ShopListViewModel @Inject constructor(
+    private val addBill: AddBillItemUseCase,
     private val getTypeList: GetTypeUseCase
 ) : ViewModel() {
 
@@ -23,6 +21,25 @@ class ShopListViewModel @Inject constructor(
 
     fun getNoteList() {
         list =  getTypeList.invoke(TYPE_NOTE)
+    }
+
+    suspend fun add(textNote: String, color: String){
+        addBill.invoke(newItem(0, textNote, color))
+    }
+
+    fun newItem(id: Int, textNote: String, color: String): BillsItem {
+        return BillsItem(
+            id,
+            TYPE_NOTE,
+            "",
+            "",
+            "",
+            "",
+            "",
+            textNote,
+            color,
+            false, "", "", "", "", ""
+        )
     }
 
 }
