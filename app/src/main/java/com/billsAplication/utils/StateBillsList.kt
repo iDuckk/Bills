a3696eefc395183be8bd9638caff375a35cd78c2
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-12-29 21:20:34 +0700

    Version 7.11.5
    - Изменил StateColorButton.kt
    - Добавил State в BillsListViewModel.kt

--- app/src/main/java/com/billsAplication/utils/StateBillsList.kt
+++ app/src/main/java/com/billsAplication/utils/StateBillsList.kt
@@ -7,3 +7,4 @@ sealed class StateBillsList
 class Error(val exception: String): StateBillsList()
 class TotalAmountBar(val exp: String = "0.0", val inc: String = "0.0", val tot: String = "0.0") : StateBillsList()
 class Result(val list: ArrayList<BillsItem>) : StateBillsList()
+class ColorState(val type: Int) : StateBillsList()
