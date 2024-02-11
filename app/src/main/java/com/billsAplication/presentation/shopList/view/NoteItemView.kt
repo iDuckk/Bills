be91e65ec06ce5b12051b2cdf08e9b9a7cbeb477
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2024-02-11 17:15:06 +0700

    Version 4.0.14
    - AutoLink NoteItemView
    - Change NoteItemView

--- app/src/main/java/com/billsAplication/presentation/shopList/view/NoteItemView.kt
+++ app/src/main/java/com/billsAplication/presentation/shopList/view/NoteItemView.kt
@@ -1,16 +1,26 @@
 package com.billsAplication.presentation.shopList.view
 
+import android.text.Layout.Alignment
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
+import android.util.Patterns
+import android.view.View.TEXT_ALIGNMENT_CENTER
+import android.widget.TextView
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -18,7 +28,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.util.LinkifyCompat
+import com.billsAplication.R
 import com.billsAplication.domain.model.NoteItem
 import com.billsAplication.extension.getColorFromAttr
 
@@ -30,9 +42,12 @@ fun NoteItemView(
 ) {
 
     val context = LocalContext.current
+    val customLinkifyTextView = remember {
+        TextView(context)
+    }
 
     Card(
-        shape = RectangleShape,
+        shape = RoundedCornerShape(13.dp),
         border = BorderStroke(width = 1.dp, color = Color.LightGray),
         colors = CardDefaults.cardColors(
             containerColor = if (note.color == "")
@@ -50,19 +65,37 @@ fun NoteItemView(
             }
     ) {
         Row(
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
                 .padding(bigDp)
         ) {
-            Text(
-                text = note.textNote,
-                color = Color.Gray,
-                fontSize = 18.sp,
+            AndroidView(
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 1.dp)
-            )
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                factory = { customLinkifyTextView }
+            ) { textView ->
+                textView.textSize = 24f
+                textView.textAlignment = TEXT_ALIGNMENT_CENTER
+                textView.setLinkTextColor(context.getColor(R.color.text_income))
+                textView.text = note.textNote ?: ""
+                LinkifyCompat.addLinks(textView, Linkify.WEB_URLS)
+                Linkify.addLinks(textView, Patterns.WEB_URL, "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+                        + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+                        + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)")
+                textView.movementMethod = LinkMovementMethod.getInstance()
+            }
+//            Text(
+//                text = note.textNote,
+//                color = Color.Gray,
+//                fontSize = 18.sp,
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(end = 1.dp)
+//            )
         }
     }
 }
